bb96dfd8a959b54ef3343144389966136c0bad42
Renan Oliveira <renanlido@yahoo.com.br>
2025-04-07 02:17:51 -0300

    feat: enhance routing by preserving navigation state in PrivateRoute and PublicRoute

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,11 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export function PrivateRoute() {
 	const { isAuthenticated } = useAuth();
+	const location = useLocation();
 
 	if (!isAuthenticated) {
-		return <Navigate to="/login" replace />;
+		return <Navigate to="/login" state={{ from: location }} replace />;
 	}
 
 	return <Outlet />;
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,11 +1,14 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export function PublicRoute() {
 	const { isAuthenticated } = useAuth();
+	const location = useLocation();
+
+	const from = location.state?.from || { pathname: "/tasks" };
 
 	if (isAuthenticated) {
-		return <Navigate to="/tasks" replace />;
+		return <Navigate to={from} replace />;
 	}
 
 	return <Outlet />;
