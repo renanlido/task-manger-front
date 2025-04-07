603716ce6223f7b38a9b236400c33b4b016160fe
Renan Oliveira <renanlido@yahoo.com.br>
2025-04-07 02:17:40 -0300

    feat: add custom hook useUrlFilter for managing task status filter in URL

---
+++ src/hooks/useUrlFilter.ts
@@ -0,0 +1,35 @@
+import { TaskStatus, TaskStatusEnum } from '@/types';
+import { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+
+interface UseUrlFilterProps {
+  setFilter: (filter: TaskStatus) => void;
+}
+
+export function useUrlFilter({ setFilter }: UseUrlFilterProps) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  const updateFilterInUrl = (value: string) => {
+    setSearchParams((params) => {
+      params.set("filter", value);
+      return params;
+    }, { replace: true });
+
+    setFilter(value as TaskStatus);
+  };
+
+  useEffect(() => {
+    const urlFilter = searchParams.get("filter") as TaskStatus;
+    if (urlFilter) {
+      setFilter(urlFilter);
+    } else if (location.search === "") {
+      setFilter(TaskStatusEnum.ALL);
+    }
+  }, [location.search, searchParams, setFilter]);
+
+  return {
+    urlFilter: (searchParams.get("filter") as TaskStatus) || "all",
+    updateFilterInUrl
+  };
+}
